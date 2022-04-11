dbed94451d847bd15503aee138128976de5af15a
hiraoka <shobu.hiraoka@digglue.com>
2022-04-11 17:31:20 +0900

    update query sample

--- index.ts
+++ index.ts
@@ -1,34 +1,63 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-	await prisma.user.create({
-	  data: {
-		name: 'Alice',
-		email: 'alice@prisma.io',
-		posts: {
-		  create: { title: 'Hello World' },
-		},
-		profile: {
-		  create: { bio: 'I like turtles' },
-		},
-	  },
-	})
-  
-	const allUsers = await prisma.user.findMany({
-	  include: {
-		posts: true,
-		profile: true,
-	  },
-	})
-	console.dir(allUsers, { depth: null })
-  }
+  // table user create sample
+  await prisma.user.create({
+    data: {
+      name: "new",
+      email: "name@hoge.com",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like Node.js" },
+      },
+    },
+  });
+
+  // table user update sample
+  await prisma.user.update({
+    data: {
+      name: "digglue_member",
+      email: "member@digglue.com",
+      posts: {
+        create: { title: "Hello digglue" },
+      },
+      profile: {
+        update: { bio: "テクノロジーで持続可能な世界を実装する" },
+      },
+    },
+    where: {
+      id: 1,
+    },
+  });
+
+  // table post update sample
+  await prisma.post.update({
+    data: {
+      title: "Hello sample update",
+    },
+    where: {
+      id: 1,
+    },
+  });
+
+  // find from user sample
+  const usersWithPostsProfile = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(usersWithPostsProfile, { depth: null });
+}
 
 main()
   .catch((e) => {
-    throw e
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
